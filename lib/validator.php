7f99930f09c4df77c22eb23b004111a398625611
ndigual <100208500+ndigual@users.noreply.github.com>
2022-06-21 20:51:48 +0000

    modification

--- lib/validator.php
+++ lib/validator.php
@@ -7,13 +7,13 @@ function est_entier($valeur) {
     return is_numeric($valeur);
 }
 
-function valid_champ(array &$arrayError, $valeur, string $key) {
+/* function valid_champ(array &$arrayError, $valeur, string $key) {
     if (est_vide($valeur)) {
         $arrayError[$key] = "Champ obligatoire";
     } else if (!est_entier($valeur)) {
         $arrayError[$key] = "Veuillez saisir un nombre";
     }
-}
+} */
 
 function valid_champ_user(array &$arrayError, $valeur, string $key) {
     if (est_vide($valeur)) {
